941de03358f369efd0f19c50014c29a2989dd0d2
Fabricio <fabricio@codeforce.com.br>
2026-04-07 14:43:52 -0300

    Ajustes para testes Github

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,11 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+        
+        // Isso impede que o Laravel procure pelo manifest.json nos testes
+        $this->withoutVite();
+    }
 }
